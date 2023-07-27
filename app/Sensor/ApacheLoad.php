5cb3600391eb65161e86a52a01e93b1b08634986
ElHathout <iliaselhathout@hotmail.com>
2023-07-27 20:57:22 +0200

    bugs fixed

--- app/Sensor/ApacheLoad.php
+++ app/Sensor/ApacheLoad.php
@@ -25,7 +25,7 @@ class ApacheLoad extends AbstractSensor
                 . "\nServer Load: ". $ApacheData["load"]
                 . "\nTotal accesses: ". $ApacheData["accesses"]
                 . "\nTotal traffic: ". $ApacheData["traffic"]
-                . "\Total duration: ". $ApacheData["uptime"]
+                . "\nTotal duration: ". $ApacheData["uptime"]
                 . "\nCPU usage: ". $ApacheData["CPULoad"]
                 . "\nRequests per seconds: ". $ApacheData["requests/s"]
                 . "\nBytes per second: ". $ApacheData["B/second"]
@@ -53,26 +53,26 @@ class ApacheLoad extends AbstractSensor
         $result = [];
         $lines = explode("\n", $string);
         for ($i = 1; $i < count($lines); $i++) {
-            if (str_contains($lines[i], 'Server Version')){
-                $result["version"] = substr($lines[i], 32, 6);
+            if (str_contains($lines[$i], 'Server Version')){
+                $result["version"] = substr($lines[$i], 31, 6);
             }
-            if (str_contains($lines[i], 'Server uptime')){
-                $result["uptime"] = substr($lines[i], 21, 34);
+            if (str_contains($lines[$i], 'Server uptime')){
+                $result["uptime"] = substr($lines[$i], 20, 36);
             }
-            if (str_contains($lines[i], 'Server load')){
-                $result["load"] = substr($lines[i], 18, 14);
+            if (str_contains($lines[$i], 'Server load')){
+                $result["load"] = substr($lines[$i], 17, 13);
             }
-            if (str_contains($lines[i], 'Total accesses')){
-                $result["accesses"] = substr($lines[i], 21, 2);
-                $result["traffic"] = substr($lines[i], 41, 4);
-                $result["duration"] = substr($lines[i], 65, strlen($lines[i])-4);
+            if (str_contains($lines[$i], 'Total accesses')){
+                $result["accesses"] = substr($lines[$i], 21, 2);
+                $result["traffic"] = substr($lines[$i], 41, 5);
+                $result["duration"] = substr($lines[$i], 65, strlen($lines[$i])-2);
             }
-            if (str_contains($lines[i], 'CPU Usage')){
-                $result["CPULoad"] = preg_split('/\s+/', explode("-", $lines[i])[1])[0];
+            if (str_contains($lines[$i], 'CPU Usage')){
+                $result["CPULoad"] = preg_split('/\s+/', explode("-", $lines[$i])[1])[1];
             }
-            if (str_contains($lines[i], 'requests/sec')){
-                $splittedLine =  preg_split('/\s+/',  $lines[i]);
-                $result["requests/s"] = $splittedLine[0];
+            if (str_contains($lines[$i], 'requests/sec')){
+                $splittedLine =  preg_split('/\s+/',  $lines[$i]);
+                $result["requests/s"] = substr($splittedLine[0], 4, strlen($splittedLine[0])-4);
                 $result["B/second"] = $splittedLine[3];
                 $result["B/request"] = $splittedLine[6];
                 $result["ms/Request"] = $splittedLine[9];
